a2982d5e5959ad847f6728330d02cbb85743a239
Anatoy Nosov <jomeisama@gmail.com>
2018-03-26 11:58:14 +0300

    GCD

---
+++ 02/gcd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int gcd_naive(int a, int b) {
+  int current_gcd = 1;
+  for (int d = 2; d <= a && d <= b; d++) {
+    if (a % d == 0 && b % d == 0) {
+      if (d > current_gcd) {
+        current_gcd = d;
+      }
+    }
+  }
+  return current_gcd;
+}
+
+int gcd_fast(int a, int b) {
+  int r = a % b;
+  if(r == 0) { return b; }
+  return gcd_fast(b, r);
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << gcd_fast(a, b) << std::endl;
+  return 0;
+}
