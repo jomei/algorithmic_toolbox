a9ac17cdc81f91351207ab28f57d7672551724db
Anatoy Nosov <jomeisama@gmail.com>
2018-03-26 16:25:22 +0300

    LCM

---
+++ 02/lcm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+long long lcm_naive(int a, int b) {
+  for (long l = 1; l <= (long long) a * b; ++l)
+    if (l % a == 0 && l % b == 0)
+      return l;
+
+  return (long long) a * b;
+}
+
+int gcd_fast(int a, int b) {
+  int r = a % b;
+  if(r == 0) { return b; }
+  return gcd_fast(b, r);
+}
+
+long long lcm_fast(int a, int b) {
+    return a * b / gcd_fast(a, b);
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << lcm_fast(a, b) << std::endl;
+  return 0;
+}
