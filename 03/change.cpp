49d7ce3a6573a3590433b0f938e2735d3e7d117f
Anatoy Nosov <jomeisama@gmail.com>
2018-04-02 17:34:37 +0300

    Change

---
+++ 03/change.cpp
@@ -0,0 +1,27 @@
+//The goal in this problem is to  nd the minimum number of coins needed to 
+//change the input value (an integer) into coins with denominations 1, 5, and 10.
+
+#include <iostream>
+
+int get_fraction(int m) {
+	int fractions[3] = {10, 5, 1};
+	for(int i = 0; i < 3; ++i) {
+		if(fractions[i] < m) { return fractions[i]; }
+	}
+}
+
+
+int get_change(int m) {
+  int n = 0;
+  while(m != 0) {
+  	m -= get_fraction(m);
+  	++n;
+  }
+  return n;
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
