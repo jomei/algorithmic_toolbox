8cc29fc6397d27502df3fe4b907f57edab747d29
Anatoy Nosov <jomeisama@gmail.com>
2018-04-19 22:57:26 +0300

    Charge dp

--- 05/primitive_calculator.cpp
+++ 05/primitive_calculator.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using std::vector;
 
 vector<int> optimal_sequence(int n) {
   std::vector<int> sequence = {1};
-  vector<int> storage = {0};
+
+  std::map<int, vector<int>> storage;
+  storage[1] = {1};
   std::vector<int> results;
-  while(sequence.back() != n) {
-    
+  int count = 1;
+  int p1, x2, x3;
+  while(storage.count(n) == 0) {
+      p1 = count + 1;
+      x2 = count * 2;
+      x3 = count * 3;
+      if(storage.count(p1) == 0) {
+        std::vector<int> tmp = storage.find(count)->second;
+        tmp.push_back(p1);
+        storage[p1] = tmp;
+      }
+      if(storage.count(x2) == 0) {
+        std::vector<int> tmp = storage.find(count)->second;
+        tmp.push_back(x2);
+        storage[x2] = tmp;
+      }
+      if(storage.count(x3) == 0) {
+        std::vector<int> tmp = storage.find(count)->second;
+        tmp.push_back(x3);
+        storage[x3] = tmp;
+      }
+
   }
-  return sequence;
+  return storage.find(n)->second;
 }
 
 int main() {
