feaae9c33058ab4468e3a950912fe0944ea111d2
Anatoy Nosov <jomeisama@gmail.com>
2018-05-10 17:33:30 +0300

    Partition 3

--- 06/partition3.cpp
+++ 06/partition3.cpp
@@ -9,27 +9,39 @@ int partition3(vector<int> &A) {
   if(sum % 3 != 0) {return 0;}
   sum = sum / 3;
   bool table[A.size() +1][sum + 1][sum + 1];
+
+  for (int i =0; i <=A.size(); ++i) {
+  	for(int j = 0;j <=sum; ++j) {
+  		for(int k = 0; k <=sum; ++k) {
+  			table[i][j][k]= false;
+  		}
+  	}
+  }
   for (int i = 0; i <= A.size(); ++i) {
     table[i][0][0] = true;
   }
 
-  for (int i = 1; i <= sum; i++) {
+  for (int i = 1; i <= sum; ++i) {
       table[0][i][0] = false;
       table[0][0][i] = false;
   }
 
   table[0][0][0] = true;
 
-  for (int k = 1; k <= A.size(); k++) {
-      for (int i = 0; i <= sum; i++) {
-          for (int j = 0; j <= sum; j++) {
+  for (int k = 1; k <= A.size(); ++k) {
+      for (int i = 0; i <= sum; ++i) {
+          for (int j = 0; j <= sum; ++j) {
               table[k][i][j] = table[k - 1][i][j] ||
                       (i >= A[k - 1] && table[k - 1][i - A[k - 1]][j]) ||
                       (j >= A[k - 1] && table[k - 1][i][j - A[k - 1]]);
           }
       }
   }
-  return table[A.size()][sum][sum] ? 1 : 0;
+  if(table[A.size()][sum][sum]) {
+  	return 1;
+  } else{
+  	return 0;
+  }
 }
 
 int main() {
@@ -39,5 +51,6 @@ int main() {
   for (size_t i = 0; i < A.size(); ++i) {
     std::cin >> A[i];
   }
-  std::cout << partition3(A) << '\n';
+  int a =  partition3(A);
+  std::cout << a << '\n';
 }
