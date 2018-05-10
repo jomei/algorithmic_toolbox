8eed953a9056eed87ae2db31be62a2056d3b2b64
Anatoy Nosov <jomeisama@gmail.com>
2018-05-10 17:34:07 +0300

    Lcs2

--- 05/lcs2.cpp
+++ 05/lcs2.cpp
@@ -10,7 +10,28 @@ int step(vector<int> &s1, vector<int> &s2, int i, int j) {
 }
 
 int lcs2(vector<int> &s1, vector<int> &s2) {
-  return step(s1, s2, s1.size(), s2.size());
+    vector<vector<int>> L;
+ 
+    for (int i=0; i<=s1.size(); ++i)
+    {   
+        L.push_back({});
+        for (int j=0; j<=s2.size(); ++j)
+        {
+
+                if (i == 0 || j == 0)
+                    L[i].push_back(0);
+ 
+                else if (s1[i-1] == s2[j-1])
+                    L[i].push_back(L[i-1][j-1] + 1);
+ 
+                else
+                    L[i].push_back(std::max(L[i-1][j],
+                                         L[i][j-1]));
+          
+        }
+    }
+ 
+    return L[s1.size()][s2.size()];
 }
 
 
